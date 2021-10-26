addd985a0eb00c06c4104cc4101056712b8cb3b8
EdanPotter <a.f.past@hotmail.com>
2021-10-26 11:25:42 -0300

    Update prática 1

--- Prática 1/LoRaReceiver/src/main.cpp
+++ Prática 1/LoRaReceiver/src/main.cpp
@@ -1,34 +1,67 @@
-#include <Arduino.h>
-#include "heltec.h"
+/*
+  This is a simple example show the Heltec.LoRa recived data in OLED.
 
-#define BAND 915E6  //you can set band here directly,e.g. 868E6,915E6
-void setup() {
-    //WIFI Kit series V1 not support Vext control
-    Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+  The onboard OLED display is SSD1306 driver and I2C interface. In order to make the
+  OLED correctly operation, you should output a high-low-high(1-0-1) signal by soft-
+  ware to OLED's reset pin, the low-level signal at least 5ms.
 
+  OLED pins to ESP32 GPIOs via this connecthin:
+  OLED_SDA -- GPIO4
+  OLED_SCL -- GPIO15
+  OLED_RST -- GPIO16
+  
+  by Aaron.Lee from HelTec AutoMation, ChengDu, China
+  成都惠利特自动化科技有限公司
+  www.heltec.cn
+  
+  this project also realess in GitHub:
+  https://github.com/Heltec-Aaron-Lee/WiFi_Kit_series
+*/
+#include "heltec.h" 
+
+#define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
+
+void LoRaData(){
+  Heltec.display->clear();
+  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+  Heltec.display->setFont(ArialMT_Plain_10);
+  //Heltec.display->drawString(0 , 15 , "Received "+ packSize + " bytes");
+  Heltec.display->drawString(0, 0, rssi);  
+  Heltec.display->drawString(0, 15, packet);
+  Heltec.display->display();
 }
 
-void loop()
-{
-    // try to parse packet
-    int packetSize = LoRa.parsePacket();
-    if (packetSize)
-    {
-        // received a packet
-        Serial.print("Received packet '");
-        // read packet
-        String msg = "Received: ";
-        while (LoRa.available())
-        {
-            char received = ((char)LoRa.read());
-            Serial.print(received);
-            msg = msg + received;
-        }
-        Heltec.display->clear();
-        Heltec.display->drawString(0, 0, msg);
-        Heltec.display->display();
-        // print RSSI of packet
-        Serial.print("' with RSSI ");
-        Serial.println(LoRa.packetRssi());
-    }
+void cbk(int packetSize) {
+  packet ="";
+  packSize = String(packetSize,DEC);
+  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+  rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
+  LoRaData();
+}
+
+void setup() { 
+   //WIFI Kit series V1 not support Vext control
+  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+ 
+  Heltec.display->init();
+  Heltec.display->flipScreenVertically();  
+  Heltec.display->setFont(ArialMT_Plain_10);
+  delay(1500);
+  Heltec.display->clear();
+  
+  Heltec.display->drawString(0, 0, "Heltec.LoRa Initial success!");
+  Heltec.display->drawString(0, 10, "Wait for incoming data...");
+  Heltec.display->display();
+  delay(1000);
+  //LoRa.onReceive(cbk);
+  LoRa.receive();
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) { cbk(packetSize);  }
+  delay(10);
 }
--- Prática 1/LoRaSender/src/main.cpp
+++ Prática 1/LoRaSender/src/main.cpp
@@ -1,37 +1,59 @@
 #include <Arduino.h>
 #include "heltec.h"
-#define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <SDS011.h>
 
-int counter = 0;
+#define BAND 915E6  //you can set band here directly,e.g. 868E6,915E6
+#define DHTPIN 3 //Pino responsavel por ler os dados do DHT11
+#define DHTTYPE DHT11 //Utilizamos o DHT11
+
+DHT dht(DHTPIN, DHTTYPE); //Construtor do objeto dht
+SDS011 my_sds; //construtor do objeto SDS011
+
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
+float p10, p25;
+int err,ip10,ip25;
+
+#ifdef ESP32 //Conexão HardwareSerial dos SDS
+HardwareSerial port(2);
+#endif
 
 void setup() {
-  
+  Serial.begin(9600);
   //WIFI Kit series V1 not support Vext control
-  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+  Heltec.begin(false /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+  dht.begin();
+  my_sds.begin(&port);
+  pinMode(25, OUTPUT);
 
   
 }
 
 void loop() {
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
-  // send packet
-  LoRa.beginPacket();
-/*
-* LoRa.setTxPower(txPower,RFOUT_pin);
-* txPower -- 0 ~ 20
-* RFOUT_pin could be RF_PACONFIG_PASELECT_PABOOST or RF_PACONFIG_PASELECT_RFO
-*   - RF_PACONFIG_PASELECT_PABOOST -- LoRa single output via PABOOST, maximum output 20dBm
-*   - RF_PACONFIG_PASELECT_RFO     -- LoRa single output via RFO_HF / RFO_LF, maximum output 14dBm
-*/
-  LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
-  LoRa.print("hello ");
-  LoRa.print(counter);
-  LoRa.endPacket();
+  float h = dht.readHumidity(); //lendo a umidade
+  float t = dht.readTemperature(); //lendo a temperatura
+  err = my_sds.read(&p25, &p10);
+  if (!err) {
+    Serial.println("Ok");
+    LoRa.beginPacket();
+    LoRa.print("Umi: ");
+    LoRa.print(h);
+    LoRa.println("%");
+    LoRa.print("Temp: ");
+    LoRa.print(t);
+    LoRa.println("ºC");
+    LoRa.print("PM25: ");
+    LoRa.println(p25);
+    LoRa.print("PM10: ");
+    LoRa.println(p10);
+    LoRa.endPacket(); //retorno= 1:sucesso | 0: falha
+  }
+  else{
+    Serial.println("Erro!");
+  }
   
-  counter++;
-  digitalWrite(25, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(25, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
 }
